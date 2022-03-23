4922a9ee7014bf349420b6fca70cbef111aace35
kbosk <kostyaboskin@gmail.com>
2022-03-23 15:01:30 +0200

    - Add apps to apps list - Move dependencies to requirements file - Add catalogue entities - Add static information entities

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,18 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+    category_description = models.CharField(max_length=30)
+    parent_category_id = models.ForeignKey("self", on_delete=models.CASCADE)
+
+
+class StaticItem(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    html_content = models.TextField()
+
+
+class Catalogue(models.Model):
+    categories = ArrayField(models.IntegerField(blank=True), blank=True)
--- static_information/models.py
+++ static_information/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class InformationCategory(models.Model):
+    information_cat_id = models.BigAutoField(primary_key=True)
+    category_name = models.CharField(max_length=30)
+
+
+class InformationItem(models.Model):
+    information_cat_id = models.ForeignKey(InformationCategory, on_delete=models.CASCADE)
+    content_path = models.CharField(max_length=255)
+
+
+class InlineInformationItem(models.Model):
+    information_cat_id = models.ForeignKey(InformationCategory, on_delete=models.CASCADE)
+    content_path = models.ForeignKey(InformationItem, on_delete=models.CASCADE)
+    marqup = models.TextField()
