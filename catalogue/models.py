5258efa9e4507b5c44c35f69155e57b1cf3036a0
kbosk <kostyaboskin@gmail.com>
2022-03-23 15:43:20 +0200

    - Fix category default field

--- catalogue/models.py
+++ catalogue/models.py
@@ -7,12 +7,9 @@ class Category(models.Model):
     category_name = models.CharField(max_length=30)
     category_description = models.CharField(max_length=30)
     parent_category = models.ForeignKey("self", on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
 
 
 class StaticItem(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     html_content = models.TextField()
-
-
-class Catalogue(models.Model):
-    categories = ArrayField(models.IntegerField(blank=True), blank=True)
