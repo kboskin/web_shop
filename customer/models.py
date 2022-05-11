de5781fb0b64639ed6f60492900cd04294f759fb
kbosk <kostyaboskin@gmail.com>
2022-05-11 15:21:13 +0300

    - Fix pr comments

--- customer/models.py
+++ customer/models.py
@@ -11,4 +11,4 @@ class Customer(models.Model):
 class VisitLog(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="visit_log_customer")
     activity_type = models.CharField(max_length=30)
-    activity_date = models.DateField()
+    activity_date = models.DateField(auto_now_add=True)
--- product/models.py
+++ product/models.py
@@ -9,20 +9,18 @@ from customer.models import Customer
 
 
 class Distributor(models.Model):
-    distributor_id = models.BigAutoField(primary_key=True)
-    distributor_name = models.CharField(max_length=50)
-    distributor_address = models.CharField(max_length=50)
-    distributor_phone = PhoneNumberField()
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    phone = PhoneNumberField()
 
 
 class Product(models.Model):
-    product_id = models.BigAutoField(primary_key=True)
-    product_name = models.CharField(max_length=50)
-    product_description = models.CharField(max_length=50)
-    product_price = models.IntegerField()
-    product_category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product_category_category")
-    product_distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE, related_name="product_distributor_distributor")
-    product_amount = models.IntegerField()
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product_category_category")
+    distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE, related_name="product_distributor_distributor")
+    amount = models.IntegerField()
 
 
 class Image(models.Model):
@@ -34,4 +32,5 @@ class Image(models.Model):
 class Feedback(models.Model):
     product = models.ForeignKey(Product, related_name="feedback_product", on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, related_name="feedback_customer", on_delete=models.CASCADE)
+    message = models.TextField()
     date = models.DateField()
